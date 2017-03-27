4a0c22b1b0e8b6410e96c34b8c8b2beb3b0ca677
Ferre Vidal <aferre@indra.es>
2017-03-27 16:50:17 +0200

    add files via console

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var logger = require('morgan');
+
+
+
+var routes = require('./modules/api/routes/index');
+
+
+var app = express();
+
+// view engine setup
+
+
+app.use(logger('dev'));
+
+
+app.use('/', routes);
+
+module.exports = app;
