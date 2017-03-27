22a651b8db3db22c246e77261ea139a265ba9100
albertferre <albertferrevidal@gmail.com>
2017-03-27 16:35:35 +0200

    Add files via upload

---
+++ modules/api/routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+var ctrlAranyas = require('../controllers/index');
+
+router.get('/status', ctrlAranyas.status);
+// router.get('/runaranya', function(req, res) {
+//   var nombreAranya = req.query.nombre;
+//   console.log(nombreAranya);
+//   ctrlAranyas.runAranya(nombreAranya);
+//   res.end();
+// });
+router.get('/runAranya', ctrlAranyas.runAranya);
+// router.get('/runRepsol', ctrlAranyas.runRepsol);
+// router.get('/runRepsol', ctrlAranyas.runRepsol);
+// router.get('/runRepsol', ctrlAranyas.runRepsol);
+// router.get('/runRepsol', ctrlAranyas.runRepsol);
+// router.get('/runRepsol', ctrlAranyas.runRepsol);
+// router.get('/runRepsol', ctrlAranyas.runRepsol);
+
+
+
+module.exports = router;
---
+++ settings.js
@@ -0,0 +1,7 @@
+exports.dbConfigMsSQL = {
+  user: 'sqlcsa',
+  password: 'Zaragoza1',
+  server: '40.115.26.178',
+  database: 'mahounet',
+  requestTimeout: 360000,
+};
